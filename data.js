a37e6bc6620add0f3187e19a5012cf8a8e6bc75e
RahelRegina <pushparegina0123@gmail.com>
2024-06-23 01:29:02 +0530

    route params and query strings

--- data.js
+++ data.js
@@ -2,17 +2,20 @@ const products=[
     {
         id:1,
         name:'annie',
-        age:20
+        age:20,
+        review:'hello annie'
     },
     {
         id:2,
         name:'ashwini',
-        age:20
+        age:20,
+        review:'hello ashwini'
     },
     {
         id:3,
         name:'bhagya',
-        age:20
+        age:20,
+        review:'hello bhagya'
     }
 
 ]
--- express-tutorial/jsonandparams.js
+++ express-tutorial/jsonandparams.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express()
-const {products}=require('./data')
+const {products}=require('./data');
+const { find } = require('lodash');
 
 
 app.get('/',(req,res)=>{
@@ -13,6 +14,35 @@ app.get('/products',(req,res)=>{
         return {id,name}
     })
     res.json(newProducts)
+})
+app.get('/products/:productID',(req,res)=>{
+    // console.log(req);
+    // console.log(req.params);
+    const {productID}=req.params;
+   const singleProduct=products.find((products)=>products.id===Number(productID))
+   if(!singleProduct){
+    return res.status(404).send('Product does not exist')
+   }
+    return res.json(singleProduct)
+})
+app.get('/products/:productID/reviews/:reviewID',(req,res)=>{
+    console.log(req.params);
+    res.send('Hello World')
+})
+app.get('/products/v1/query',(req,res)=>{
+    // console.log(req.query);
+    const {search,limit}=req.query;
+    let sortedProducts=[...products]
+    if(search){
+        sortedProducts=sortedProducts.filter((products)=>{
+            return products.name.startsWith(search)
+        })
+    }
+    if(limit){
+        sortedProducts=sortedProducts.slice(0,Number(limit))
+    }
+    res.status(200).json(sortedProducts)
+    res.send('Hello World')
 })
 app.listen(5000,()=>{
     console.log('Server Listening on port 5000....');
