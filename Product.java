7cadce410ad3838dacd2c9d96902585ae932c90f
RahelRegina <pushparegina0123@gmail.com>
2024-06-29 01:15:55 +0530

    Post  method with example

---
+++ Product.java
@@ -0,0 +1,105 @@
+class Product{
+
+Long id;
+
+String name;
+
+var  age;
+
+var email;
+
+var dob;
+
+}
+
+Product pr = new Product;
+
+userDetails = "";
+
+ProductTable.query(where user = userDetails);
+
+<List> product =  ProductTable.query(where user = userDetails);;
+
+pr.setname(ProductTable.getName)
+
+return pr;
+
+jdbc ---------------- >  select * from product;
+
+insert product;
+
+hibernate ----------> covert into sql ---->  db result
+
+----> java - >>java object convert
+student table in db
+id,name,age,mail,dob.... select *from student;
+
+
+@Entity
+@Table(student)
+class Studnet {
+
+    Long id
+    string name
+    int age;
+    string email
+    date dob;
+}
+
+
+student.setage(age-20);
+
+
+dao.save(studnet);  ----------------- transform into insertquery ------db la store aayidum
+
+
+
+
+
+
+class StudentDto{
+
+    name;
+    age;
+    email;
+}
+
+
+request ---- json {"name" : "ragel", "age" : "21" } -------> StudentDto ----- 
+
+StudentDto -> studnet entity -->  save(db) --- return "user created sucessfully";
+
+
+
+userCreate(){
+
+    //////
+    email(appemail, userEmail ,  "user Creation", " user create successfully");
+}
+
+
+
+otp(){
+
+
+}
+
+
+
+warnign(){
+
+    email
+}
+
+offer() {
+
+    
+}
+
+
+email(fromEmail , toEmail, Subject, Message){
+
+    sendEmail();
+
+
+}
--- app.js
+++ app.js
@@ -5,8 +5,20 @@ const {products}=require('./data')
 
 app.use(express.static('./express-post'))
 app.use(express.urlencoded({extended:false}))
+
+app.use(express.json())
+
+
 app.get('/api/products',(req,res)=>{
     res.status(200).json({success:true,data:products})
+})
+
+app.post('/api/products',(req,res)=>{
+    const {name}=req.body
+    if(!name){
+       return res.status(400).json({success:false,msg:'please provide name value'})
+    }
+    res.status(201).json({success:true,person:name})
 })
 
 app.post('/login',(req,res)=>{
@@ -17,6 +29,13 @@ app.post('/login',(req,res)=>{
     res.status(401).send('Pleasee Provide Credentials')
 })
 
+
+app.get('/nameDetails', (req, res) =>{
+
+    let names = {"name": "pushpu", "age" :21};
+    return  res.status(200).send(names);
+    
+})
 
 app.listen(5000,()=>{
     console.log('server listening on port 5000......');
